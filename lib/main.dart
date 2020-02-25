dc156f2cbaa1d8d660b1431acfb9dd275d79435d
majedict34@gmail.com <majedict34@gmail.com>
2020-02-26 01:42:06 +0600

    add row

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(MaterialApp(
-
-      home: Myapp()
-  ));
+void main() {
+  runApp(MaterialApp(home: Myapp()));
 }
 
-class Myapp extends StatelessWidget{
+class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text("new phone app"),),
+      body:Column (
+        children: <Widget>[
+          Container(
+            height: 80,
+            width: 100,
+            //decoration: BoxDecoration(color: Colors.pink),
+            foregroundDecoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris1_brown.png")
+                )
 
-      appBar: AppBar(title: Text("new phone app"),),
+            ),
+          ), Container(
+            height: 80,
+            width: 100,
+            //decoration: BoxDecoration(color: Colors.pink),
+            foregroundDecoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris1_brown.png")
+                )
 
+            ),
+          ),
+
+        ],
+      )
     );
   }
-
 }
